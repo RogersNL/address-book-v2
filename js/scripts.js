d5a9fcea5ea9337bff5ac08583d6cde9daba7282
Nick Rogers and William Kinzig <nick@>
2018-06-12 10:23:00 -0700

    Removes extra empty address forms.

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,8 @@ Address.prototype.fullAddress = function() {
   return this.addressType + ": " + this.street + ", " + this.city + ", " + this.state;
 }
 
+var emptyForm = true;
+var emptyNewForm = true;
 function resetFields() {
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
@@ -33,7 +35,7 @@ $(document).ready(function() {
 
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address">' +
-                                 '<label for="address-type">Address Type</label>' + 
+                                 '<label for="address-type">Address Type</label>' +
                                  '<select class="form-control" id="address-type">' +
                                    '<option value="Home">Home</option>' +
                                    '<option value="Work">Work</option>' +
@@ -59,18 +61,32 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
-
+    if (inputtedFirstName === "") {
+      emptyForm = true;
+    } else {
+      emptyForm = false;
+    }
     $(".new-address").each(function() {
       var inputtedAddressType = $(this).find("select#address-type").val();
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
       var newAddress = new Address(inputtedAddressType, inputtedStreet, inputtedCity, inputtedState)
-      newContact.addresses.push(newAddress)
+      if (inputtedStreet === "" || inputtedCity === "" || inputtedState === ""){
+        emptyNewForm = true;
+      } else {
+        emptyNewForm = false;
+      }
+      if (inputtedStreet === "" && emptyNewForm) {
+        $("#new-addresses .new-address:last").remove();
+      } else {
+        emptyForm = false;
+        newContact.addresses.push(newAddress)
+      }
     });
-
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
+    if(!emptyForm) {
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    }
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.fullName());
